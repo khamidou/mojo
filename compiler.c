75004786187096fbe3b56f0f099502a1695756ef
khamidou <khamidou@gmail.com>
2010-01-12 18:50:17 +0100

    added file for the boostrap compiler

---
+++ compiler.c
@@ -0,0 +1,17 @@
+/*
+  The mojo boostrap compiler.
+  copyright (c) 2009 karim hamidou.
+
+  This is a rough translator for a subset of mojo to C.
+  
+*/
+
+#include "compiler.h"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		fail("%s file", argv[0]);
+	}
+		
+}
---
+++ compiler.h
@@ -0,0 +1,8 @@
+#ifndef __COMPILER_H__
+#define __COMPILER_H__
+
+#include "tokens.h"
+#include "object.h"
+
+
+#endif
